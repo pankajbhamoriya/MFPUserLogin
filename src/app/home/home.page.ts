e62ea81fa5014a225fc7726a83829033de481e59
pankajbhamoriya <38315265+pankajbhamoriya@users.noreply.github.com>
2020-02-06 14:40:14 +0530

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,27 +9,30 @@ import { HttpClient } from '@angular/common/http';
 export class HomePage {
 
   usersList:any[]=[];
-  nodeList:any[]=[];
   data:String;
   constructor(public http: HttpClient){
     console.log("Reading json data")
-    this.http.get(
-      "https://randomuser.me/api/?results=5").subscribe(data=>{
+    this.http.get("https://randomuser.me/api/?results=5").subscribe(data=>{
 //process the json data
-console.log(this.http.get("'http://mpradip.ntpc.co.in/mfp/api/adapters/SampleHttpAdapter/Alerts?params=['Severity=3','','']"))
 console.log(data)
 this.data=JSON.stringify(data)
-this.usersList=data["results"]
-})
+this.usersList=data["results"]})
 
-this.http.get(
-  "http://mpradip.ntpc.co.in/mfp/api/adapters/SampleHttpAdapter/Alerts?params=['Severity=3','','']").subscribe(data=>{
-//process the json data
-console.log(this.http.get("http://mpradip.ntpc.co.in/mfp/api/adapters/SampleHttpAdapter/Alerts?params=['Severity=3','','']"))
-this.data=JSON.parse(<string>data);
-this.nodeList=data["rows"]
-console.log(this.nodeList)
-})
+var resourceRequest = new WLResourceRequest(
+  "/adapters/SampleHttpAdapter/Alerts",
+  WLResourceRequest.GET
+);
+
+resourceRequest.setQueryParameter("params", "['Severity=3','','']");
+
+resourceRequest.send().then(
+  function(response) {
+      alert(JSON.stringify(response));
+  },
+  function(error) {
+      alert(JSON.stringify(error));
+  }
+);
 }
 
 }
